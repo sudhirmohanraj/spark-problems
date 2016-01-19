fd8837b77a81b413a6590d2cb112e6f052b50997
Mohanraj, Sudhir <sudhir.mohanraj@capitalone.com>
2016-01-18 21:28:42 -0500

    Solution to find popular routes

---
+++ src/main/scala/PopularRoutes.scala
@@ -0,0 +1,14 @@
+import org.apache.spark.SparkContext
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+
+object SimpleApp {
+  def main(args: Array[String]) {
+    val conf = new SparkConf().setAppName("Calculates the popular routes")
+    val sc = new SparkContext(conf)
+    val textFile = sc.textFile("Car_Routes.txt")
+    val routeMap = textFile.flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey((a, b) => a + b)
+    	println("Car_Routes")
+	routeMap foreach {case (key, value) => println (key + "-->" + value)}
+  }
+}
